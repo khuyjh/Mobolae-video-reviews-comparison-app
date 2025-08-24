03cb5e816e6e84563cc0f4ac532f87d7342b406a
이관현\Luganic <rhksgus509@gmail.com>
2025-08-25 00:30:14 +0900

    [HOY-28] feat: Buttons 로직 구현(이관현)

--- src/shared/components/card/productCard/productButtons.tsx
+++ src/shared/components/card/productCard/productButtons.tsx
@@ -1,1 +1,14 @@
 // 하단 버튼들
+import React from 'react';
+
+const ProductButtons = () => {
+  return (
+    <div>
+      <button>리뷰 작성하기</button>
+      <button>비교하기</button>
+      <button>편집하기</button>
+    </div>
+  );
+};
+
+export default ProductButtons;
