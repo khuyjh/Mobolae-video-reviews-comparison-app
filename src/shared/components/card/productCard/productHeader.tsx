be262c5239a5810430a20df86979d3b918a35120
이관현\Luganic <rhksgus509@gmail.com>
2025-08-24 23:56:13 +0900

    [HOY-28] feat: Header 로직 구현(이관현)

--- src/shared/components/card/productCard/productHeader.tsx
+++ src/shared/components/card/productCard/productHeader.tsx
@@ -1,1 +1,23 @@
 // 칩 + 제목 + 조회수
+
+import React from 'react';
+
+import { Chip } from '../../chip';
+
+interface ProductHeaderProps {
+  chipLabel: string;
+  title: string;
+  views: number;
+}
+
+const ProductHeader = ({ chipLabel, title, views }: ProductHeaderProps) => {
+  return (
+    <div>
+      <Chip>{chipLabel}</Chip>
+      <h2>{title}</h2>
+      <p>조회 {views}</p>
+    </div>
+  );
+};
+
+export default ProductHeader;
