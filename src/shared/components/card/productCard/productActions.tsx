88538ca6763d0c47041c97cc163e0a98ba194467
이관현\Luganic <rhksgus509@gmail.com>
2025-08-25 00:21:16 +0900

    [HOY-28] feat: Actions 로직 구현(이관현)

--- src/shared/components/card/productCard/productActions.tsx
+++ src/shared/components/card/productCard/productActions.tsx
@@ -1,1 +1,25 @@
 // 좋아요 + 카카오 공유 + 클립보드 복사
+import { Heart } from 'lucide-react';
+import React from 'react';
+
+import KakaotalkIcon from '../../../../../public/icons/kakaotalk.svg';
+import ShareIcon from '../../../../../public/icons/share.svg';
+
+const ProductActions = () => {
+  <div>
+    <div>
+      <button>
+        <KakaotalkIcon />
+      </button>
+      <button>
+        <ShareIcon />
+      </button>
+    </div>
+
+    <button>
+      <Heart />
+    </button>
+  </div>;
+};
+
+export default ProductActions;
