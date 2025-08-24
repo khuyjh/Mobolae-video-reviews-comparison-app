81f27e3082fa67ed78fe6fc04a2444c601829ee9
이관현\Luganic <rhksgus509@gmail.com>
2025-08-25 00:26:45 +0900

    [HOY-28] feat: Description  로직 구현(이관현)

--- src/shared/components/card/productCard/productDescription.tsx
+++ src/shared/components/card/productCard/productDescription.tsx
@@ -1,1 +1,12 @@
-// 설명
+// 콘텐츠 설명
+import React from 'react';
+
+interface ProductDescriptionProps {
+  description: string;
+}
+
+const ProductDescription = ({ description }: ProductDescriptionProps) => {
+  return <p>{description}</p>;
+};
+
+export default ProductDescription;
