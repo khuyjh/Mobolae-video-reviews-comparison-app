a87f5bf5bd7882825d7011dfef3e5d866ec249a9
DESKTOP-BDRHND2\배혜민 <phsoo1997@daum.net>
2025-08-27 12:09:41 +0900

    [HOY-22] fix: QA 수정사항 반영

--- src/shared/components/GlobalNav.tsx
+++ src/shared/components/GlobalNav.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { cn } from '@/shared/lib/cn';
 
@@ -10,12 +10,34 @@ export default function GlobalNav() {
   const [searchOpen, setSearchOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const triggerBtnRef = useRef<HTMLButtonElement>(null);
+  const mobileSearchRef = useRef<HTMLDivElement>(null);
+
+  {
+    /* 모바일 검색창 닫힘*/
+  }
+  useEffect(() => {
+    if (!searchOpen) return;
+
+    const onMouseDown = (e: MouseEvent) => {
+      const t = e.target;
+      if (!(t instanceof Node)) return;
+      const inSearch = mobileSearchRef.current?.contains(t);
+      const inTrigger = triggerBtnRef.current === t || triggerBtnRef.current?.contains(t);
+
+      if (!inSearch && !inTrigger) setSearchOpen(false);
+    };
+
+    document.addEventListener('mousedown', onMouseDown);
+    return () => document.removeEventListener('mousedown', onMouseDown);
+  }, [searchOpen]);
+
   return (
-    <header className='top-0 z-40 md:sticky md:top-[36px]'>
+    <header className='sticky z-40 border border-[var(--color-black-800)] bg-[var(--color-black-900)] py-[11px] pb-[11px] md:py-[15px] md:pb-[15px] xl:py-[22px] xl:pb-[22px]'>
       <nav className={NAV_CONTAINER}>
         {/*로고*/}
         <Link href='/' className='flex items-center'>
-          <img src='/icons/Logo.svg' alt='로고' className='h-[28px] md:h-[56px]' />
+          <img src='/icons/Logo.svg' alt='로고' className='h-[28px] md:h-[46px] xl:h-[56]' />
         </Link>
         {/*PC 검색창*/}
         <div className='hidden md:flex md:items-center'>
@@ -33,19 +55,19 @@ export default function GlobalNav() {
           {/*PC 오른쪽 메뉴 / 로그인 상태 -> 비교하기, 내 프로필 / 로그아웃 상태 -> 로그인, 회원가입*/}
           {isLoggedIn ? (
             <div className={DESKTOP_MENU}>
-              <Link href='/compare' className='hover:text-[var(--color-white)]'>
+              <Link href='/compare' className='hover:text-[var(--color-gray-600)]'>
                 비교하기
               </Link>
-              <Link href='/mypage' className='hover:text-[var(--color-white)]'>
+              <Link href='/mypage' className='hover:text-[var(--color-gray-600)]'>
                 내 프로필
               </Link>
             </div>
           ) : (
             <div className={DESKTOP_AUTH}>
-              <Link href='/signin' className='hover:text-[var(--color-white)]'>
+              <Link href='/signin' className='hover:text-[var(--color-gray-600)]'>
                 로그인
               </Link>
-              <Link href='/signup' className='hover:text-[var(--color-white)]'>
+              <Link href='/signup' className='hover:text-[var(--color-gray-600)]'>
                 회원가입
               </Link>
             </div>
@@ -57,6 +79,7 @@ export default function GlobalNav() {
           {!searchOpen && (
             <>
               <button
+                ref={triggerBtnRef}
                 className='grid h-[24px] w-[24px] place-items-center'
                 onClick={() => setSearchOpen(true)}
                 aria-label='검색 열기'
@@ -74,27 +97,36 @@ export default function GlobalNav() {
         </div>
 
         {/*모바일 검색창*/}
-        {searchOpen && (
-          <div className='md:hidden'>
-            <div className={MOBILE_SEARCH_BOX}>
-              <button className='h-[24px] w-[24px] place-items-center' aria-label='검색'>
-                <img src='/icons/SearchIcon.svg' alt='검색' />
-              </button>
-              <input
-                type='text'
-                placeholder='상품 이름을 검색해 보세요'
-                className='ml-[15px] w-full rounded-md bg-transparent outline-none'
-              />
-            </div>
+        <div className={cn('md:hidden', searchOpen ? 'static' : 'absolute')}>
+          <div
+            ref={mobileSearchRef}
+            className={cn(
+              MOBILE_SEARCH_BOX,
+              'overflow-hidden md:hidden',
+              'transition-[width] duration-300',
+              searchOpen
+                ? 'pointer-events-auto w-[calc(100%_-_60px)] ease-out'
+                : 'pointer-events-none w-0 opacity-0 ease-in',
+            )}
+          >
+            <button className='h-[24px] w-[24px] place-items-center' aria-label='검색'>
+              <img src='/icons/SearchIcon.svg' alt='검색' />
+            </button>
+            <input
+              type='text'
+              placeholder='상품 이름을 검색해 보세요'
+              className='ml-[15px] w-full rounded-md bg-transparent outline-none'
+            />
+          </div>
+          {searchOpen && (
             <button
               className='ml-[20px] grid h-[24px] w-[24px] place-items-center'
-              onClick={() => setSearchOpen(false)}
-              aria-label='검색 닫기'
+              aria-label='메뉴 열기'
             >
               <img src='/icons/MenuIcon.svg' alt='메뉴' />
             </button>
-          </div>
-        )}
+          )}
+        </div>
       </nav>
     </header>
   );
