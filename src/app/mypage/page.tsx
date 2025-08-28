6f7fedb98c22194b8fb94127054e7cb53d99eeec
DESKTOP-BDRHND2\배혜민 <phsoo1997@daum.net>
2025-08-29 05:57:56 +0900

    [HOY-22] fix: 아바타 카드 컴포넌트 수정사항(chip 컴포넌트 사용 / css 스타일 수정 )  반영

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -3,41 +3,62 @@
 import React from 'react';
 
 import ProfileBadge from '@/shared/components/card/avtataCard';
+import { buildRankingMap } from '@/shared/utils/rankingUtil';
+
 const AVATAR = '/images/profileImg.jpg';
+
+const items = [
+  { id: 101, name: '리뷰왕', avatarSrc: AVATAR, followers: 682, reviewCount: 398, rating: 5 },
+  { id: 102, name: '리뷰왕', avatarSrc: AVATAR, followers: 682, reviewCount: 398, rating: 4 },
+  {
+    id: 103,
+    name: '리뷰왕리뷰왕리뷰왕리뷰왕',
+    avatarSrc: AVATAR,
+    followers: 800,
+    reviewCount: 38,
+    rating: 3,
+  },
+  {
+    id: 105,
+    name: '리뷰왕리뷰왕리뷰왕리뷰왕',
+    avatarSrc: AVATAR,
+    followers: 800,
+    reviewCount: 38,
+    rating: 3,
+  },
+];
+
 const MyPage = () => {
+  const rankingMap = buildRankingMap(items);
+
   return (
     <div className='space-y-4 px-[20px]'>
-      {/*메인페이지에 들어 갈 아바타 카드*/}
+      {/* 메인페이지 랭킹 카드: rank 대신 id + rankingMap 전달 */}
+      {items.map((item) => (
+        <ProfileBadge
+          key={item.id}
+          variant='ranking'
+          id={item.id}
+          name={item.name}
+          avatarSrc={item.avatarSrc}
+          followers={item.followers}
+          review={item.reviewCount}
+          rating={item.rating}
+          rankingMap={rankingMap} // 부모에서 만든 맵
+        />
+      ))}
+
+      {/* 리뷰페이지 카드 */}
       <ProfileBadge
-        variant='ranking'
-        name='리뷰왕'
+        variant='review_profile'
+        id={999}
+        name='리뷰왕코'
         avatarSrc={AVATAR}
-        rank={1}
-        followers={682}
-        review={398}
-      />
-      <ProfileBadge
-        variant='ranking'
-        name='리뷰왕'
-        avatarSrc={AVATAR}
-        rank={2}
-        followers={682}
-        review={398}
-      />
-      <ProfileBadge
-        variant='ranking'
-        name='리뷰왕리뷰왕리뷰왕리뷰왕'
-        avatarSrc={AVATAR}
-        rank={3}
-        followers={800}
-        review={38}
+        rating={3}
       />
 
-      {/*리뷰페이지 카드*/}
-      <ProfileBadge variant='review_profile' name='리뷰왕' avatarSrc={AVATAR} rating={5} />
-
-      {/*팔로워 목록 카드 */}
-      <ProfileBadge variant='follower' name='미끄럼틀' avatarSrc={AVATAR} />
+      {/* 팔로워 목록 카드 */}
+      <ProfileBadge variant='follower' id={1000} name='미끄럼틀' avatarSrc={AVATAR} />
     </div>
   );
 };
