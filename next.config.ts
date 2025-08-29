4e1bb099a7eaf05ecdec57373e01b18a4c5202d8
이관현\Luganic <rhksgus509@gmail.com>
2025-08-30 01:10:24 +0900

    [HOY-29] chore: 최신화(이관현)

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,11 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
-        pathname: '/**', // 경로 패턴, 모든 이미지 허용
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
       },
     ],
   },
