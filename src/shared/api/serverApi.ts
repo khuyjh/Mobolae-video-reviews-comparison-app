618e67a3e8e257cb3965869903effdc06c0dc2fb
PC2502 <zxcv530@naver.com>
2025-08-29 22:27:44 +0900

    [HOY-120] feat: fetch api 인스턴스 제작 (윤정환)

---
+++ src/shared/api/serverApi.ts
@@ -0,0 +1,61 @@
+import { cookies } from 'next/headers';
+
+//fetch api 헤더에 토큰부여
+const serverFetch = async (endpoint: string, options?: RequestInit) => {
+  const accessToken = (await cookies()).get('accessToken')?.value;
+
+  //option에 header key가 있다면 추가
+  const headers = new Headers(options?.headers);
+  headers.set('Content-Type', 'application/json');
+
+  console.log('여기');
+  if (accessToken) {
+    headers.set('Authorization', `Bearer ${accessToken}`);
+  }
+  console.log('여기');
+
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${endpoint}`, {
+    ...options,
+    headers,
+  });
+  console.log('여기');
+
+  if (!response.ok) {
+    throw new Error('Fetch API 요청 실패');
+  }
+  console.log('여기');
+
+  return response.json();
+};
+
+//토큰부여 함수를 호출하는 fetch api instance
+const serverApi = {
+  get: (endpoint: string, options?: RequestInit) => {
+    return serverFetch(endpoint, {
+      method: 'GET',
+      ...options,
+    });
+  },
+  post: <T>(endpoint: string, body: T, options?: RequestInit) => {
+    return serverFetch(endpoint, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      ...options,
+    });
+  },
+  patch: <T>(endpoint: string, body: T, options?: RequestInit) => {
+    return serverFetch(endpoint, {
+      method: 'PATCH',
+      body: JSON.stringify(body),
+      ...options,
+    });
+  },
+  delete: (endpoint: string, options?: RequestInit) => {
+    return serverFetch(endpoint, {
+      method: 'DELETE',
+      ...options,
+    });
+  },
+};
+
+export default serverApi;
