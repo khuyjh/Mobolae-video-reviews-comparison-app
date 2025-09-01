0f773635899b9efeed0d9f180fb7b20cbafd854a
PC2502 <zxcv530@naver.com>
2025-09-01 21:00:27 +0900

    [HOY-123] chore: 주석 설명 추가 (윤정환)

--- src/shared/components/BaseModal.tsx
+++ src/shared/components/BaseModal.tsx
@@ -9,6 +9,8 @@ import { cn } from '../lib/cn';
 title: 해당 모달이 무엇인지 알려주는 aria-label의 역할을 합니다.
 shadcn의 규칙에 따라서 title을 반드시 포함하고, sr-only처리를 해서 ui에는 표시되지 않도록 합니다.
 
+closeOnOutsideClick: 모달 밖 overlay를 눌렀을 때, 모달이 닫히는 동작을 허용할 것인지 정합니다.
+
 onClose: 모달 밖 overlay를 눌렀을 때, 또는 x버튼을 눌렀을 때 닫히도록 isOpen을 제어하는 함수를 넘겨줍니다.
  */
 interface BaseModalProps {
