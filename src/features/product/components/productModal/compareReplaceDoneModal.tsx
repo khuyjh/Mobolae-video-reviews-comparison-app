2563e0853c4d2a006ef4652a9529eb32bbca02fa
이관현\Luganic <rhksgus509@gmail.com>
2025-09-05 01:31:11 +0900

    [HOY-54] feat: 비교 선택 모달 스타일 구현(이관현)

--- src/features/product/components/productModal/compareReplaceDoneModal.tsx
+++ src/features/product/components/productModal/compareReplaceDoneModal.tsx
@@ -1,5 +1,4 @@
 // 비교 콘텐츠 선택 후
-
 import BaseModal from '@/shared/components/BaseModal';
 import Button from '@/shared/components/Button';
 
@@ -11,8 +10,8 @@ interface Props {
 const CompareReplaceDoneModal = ({ isOpen, onClose }: Props) => {
   return (
     <BaseModal title='비교 콘텐츠 교체 완료' size='M' isOpen={isOpen} onClose={onClose}>
-      <div className='flex flex-col items-start gap-7.5 px-5 pb-5 md:gap-11 md:px-10 md:pb-10 xl:gap-10'>
-        <p className='text-xl-semibold xl:text-2xl-semibold'>
+      <div className='flex flex-col items-center gap-7.5 px-5 pb-5 md:gap-11 md:px-10 md:pb-10 xl:gap-10'>
+        <p className='text-xl-semibold xl:text-2xl-semibold text-center'>
           비교 콘텐츠가 교체되었습니다. <br />
           바로 확인해 보시겠어요?
         </p>
--- src/features/product/components/productModal/compareReplaceSelectModal.tsx
+++ src/features/product/components/productModal/compareReplaceSelectModal.tsx
@@ -1,3 +1,6 @@
+// 비교 선택 모달
+import { useState } from 'react';
+
 import BaseModal from '@/shared/components/BaseModal';
 import Button from '@/shared/components/Button';
 
@@ -9,22 +12,38 @@ interface Props {
   onChangeType: (next: CompareModalType) => void;
 }
 
+/* mock 데이터 */
+const mockProductName = '뷰티 인사이드';
+const mockCompareNameone = 'DP';
+const mockCompareNametwo = '더 글로리';
+
 const CompareReplaceSelectModal = ({ isOpen, onClose, onChangeType }: Props) => {
+  const [selected, setSelected] = useState<'A' | 'B'>('A');
+
   return (
     <BaseModal title='비교 상품 교체 선택' size='M' isOpen={isOpen} onClose={onClose}>
-      <div className='flex flex-col gap-6 p-6'>
-        <p>
-          ooo <br />
+      <div className='flex flex-col items-center gap-7.5 px-5 pb-5 md:gap-10 md:px-10 md:pb-8 xl:pb-10'>
+        <p className='text-xl-semibold xl:text-2xl-semibold text-center'>
+          지금 보신 &apos;{mockProductName}&apos;
+          <br />
           어떤 콘텐츠와 비교할까요?
         </p>
 
-        {/* 교체할 후보 버튼 2개 (임시 UI) */}
-        <div className='flex flex-col gap-3'>
-          <Button variant='secondary'>상품 A</Button>
-          <Button variant='secondary'>상품 B</Button>
+        <div className='flex w-full flex-col items-center gap-3'>
+          <Button
+            variant={selected === 'A' ? 'secondary' : 'tertiary'}
+            onClick={() => setSelected('A')}
+          >
+            {mockCompareNameone}
+          </Button>
+          <Button
+            variant={selected === 'B' ? 'secondary' : 'tertiary'}
+            onClick={() => setSelected('B')}
+          >
+            {mockCompareNametwo}
+          </Button>
         </div>
 
-        {/* 교체하기 버튼 */}
         <Button variant='primary' onClick={() => onChangeType('replaceDone')}>
           교체하기
         </Button>
