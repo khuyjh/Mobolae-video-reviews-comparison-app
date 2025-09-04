97c46c308c0454e123a830e60309ee01371f1b7a
PC2502 <zxcv530@naver.com>
2025-09-04 18:33:17 +0900

    [HOY-143] refactor: api 인스턴스를 인터셉터 기능이 있는 apiClient로 통일 (윤정환)

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,4 +1,4 @@
-import { privateApiClient, publicApiClient } from '@/shared/api/apiClients';
+import { apiClient } from '@/shared/api/apiClients';
 import { DetailUser } from '@/shared/types/userTypes';
 
 import { SignInSchema, SignUpSchema } from '../schemas/authSchema';
@@ -6,7 +6,7 @@ import { AuthResponse, KakaoLoginData, KakaoSignUpData } from '../types/authType
 
 export const signUpRequest = async (data: SignUpSchema): Promise<AuthResponse> => {
   try {
-    const res = await publicApiClient.post('/auth/signUp', data);
+    const res = await apiClient.post('/auth/signUp', data);
     return res.data;
   } catch (e) {
     console.log('회원가입 요청 에러');
@@ -16,7 +16,7 @@ export const signUpRequest = async (data: SignUpSchema): Promise<AuthResponse> =
 
 export const signInRequest = async (data: SignInSchema): Promise<AuthResponse> => {
   try {
-    const res = await publicApiClient.post('/auth/signin', data);
+    const res = await apiClient.post('/auth/signin', data);
     return res.data;
   } catch (e) {
     console.log('로그인 요청 에러');
@@ -26,7 +26,7 @@ export const signInRequest = async (data: SignInSchema): Promise<AuthResponse> =
 
 export const getMe = async (): Promise<DetailUser> => {
   try {
-    const res = await privateApiClient.get('/users/me');
+    const res = await apiClient.get('/users/me');
     return res.data;
   } catch (e) {
     console.log('사용자 정보 요청 에러');
@@ -36,7 +36,7 @@ export const getMe = async (): Promise<DetailUser> => {
 
 export const kakaoSignInRequest = async (data: KakaoLoginData): Promise<AuthResponse> => {
   try {
-    const res = await publicApiClient.post('/auth/signIn/kakao', data);
+    const res = await apiClient.post('/auth/signIn/kakao', data);
     return res.data;
   } catch (e) {
     console.log('카카오 로그인 요청 에러');
@@ -46,7 +46,7 @@ export const kakaoSignInRequest = async (data: KakaoLoginData): Promise<AuthResp
 
 export const kakaoSignUpRequest = async (data: KakaoSignUpData): Promise<AuthResponse> => {
   try {
-    const res = await publicApiClient.post('/auth/signUp/kakao', data);
+    const res = await apiClient.post('/auth/signUp/kakao', data);
     return res.data;
   } catch (e) {
     console.log('카카오 간편 회원가입 요청 에러');
--- src/shared/api/apiClients.ts
+++ src/shared/api/apiClients.ts
@@ -3,16 +3,7 @@ import Cookies from 'js-cookie';
 
 import { BASE_URL } from '../constants/constants';
 
-// 인증 토큰을 요구하지 않는 api 요청 instance
-export const publicApiClient: AxiosInstance = axios.create({
-  baseURL: BASE_URL,
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-export const privateApiClient: AxiosInstance = axios.create({
+export const apiClient: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 10000,
   headers: {
@@ -21,7 +12,7 @@ export const privateApiClient: AxiosInstance = axios.create({
 });
 
 //토큰이 필요한 요청에 대한 인터셉터
-privateApiClient.interceptors.request.use(
+apiClient.interceptors.request.use(
   (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
     const accessToken = Cookies.get('accessToken');
 
