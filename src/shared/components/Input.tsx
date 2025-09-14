87b4813b294d06361128c7f2d5bce5793fe1bb50
PC2502 <zxcv530@naver.com>
2025-09-14 18:02:22 +0900

    [HOY-178] refactor: Input과 PasswordInput을 하나의 Input 컴포넌트로 통합 (윤정환)

--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,21 +1,41 @@
 'use client';
 
-import { ComponentPropsWithRef } from 'react';
+import { ComponentPropsWithRef, useState } from 'react';
 import { FieldError } from 'react-hook-form';
 
+import VisibilityOffIcon from '../../../public/icons/VisibilityOffIcon.svg';
+import VisibilityOnIcon from '../../../public/icons/VisibilityOnIcon.svg';
 import { cn } from '../lib/cn';
 
 export interface InputProps extends ComponentPropsWithRef<'input'> {
   label?: string;
   error?: FieldError | undefined;
   helperText?: string;
+  hasButton?: boolean;
 }
 
-const BASE_INPUT_STYLE =
-  'bg-black-800 border-black-700 w-full rounded-md border-1 px-5 py-4 placeholder:text-gray-600 focus:border-2 focus:border-main focus:outline-none md:max-w-110 xl:max-w-160 xl:py-[22px]';
-
-const Input = ({ className, label, error, helperText, ...props }: InputProps) => {
+const Input = ({
+  className,
+  label,
+  error,
+  helperText,
+  hasButton = false,
+  type,
+  ...props
+}: InputProps) => {
   const inputId = props.id;
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  //password 타입과 버튼이 모두 있을 때 비밀번호 표시 토글이 동작하도록 제어
+  const getType = () => {
+    if (type !== 'password' || !hasButton) return type;
+
+    return isPasswordVisible ? 'text' : 'password';
+  };
+
+  const handleClickToggleVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
 
   return (
     <div className='text-md-regular xl:text-base-regular text-white'>
@@ -25,18 +45,30 @@ const Input = ({ className, label, error, helperText, ...props }: InputProps) =>
         </label>
       )}
 
-      <input
-        className={cn(
-          BASE_INPUT_STYLE,
-          {
-            'border-red': error,
-            'mb-[10px]': helperText || error,
-          },
-          className,
+      <div className={cn('relative', { 'mb-[10px]': helperText || error })}>
+        <input
+          className={cn(
+            BASE_INPUT_STYLE,
+            {
+              'border-red': error,
+            },
+            className,
+          )}
+          id={inputId}
+          type={getType()}
+          {...props}
+        />
+        {hasButton && (
+          <button
+            className='absolute top-[50%] right-5 translate-y-[-50%] cursor-pointer'
+            type='button'
+            onClick={handleClickToggleVisibility}
+            aria-label={isPasswordVisible ? '비밀번호 숨기기' : '비밀번호 보기'}
+          >
+            {isPasswordVisible ? <VisibilityOnIcon /> : <VisibilityOffIcon />}
+          </button>
         )}
-        id={inputId}
-        {...props}
-      />
+      </div>
 
       {error ? (
         <p className='text-red text-xs-regular xl:text-md-regular'>{error.message}</p>
@@ -46,5 +78,8 @@ const Input = ({ className, label, error, helperText, ...props }: InputProps) =>
     </div>
   );
 };
+
+const BASE_INPUT_STYLE =
+  'bg-black-800 border-black-700 w-full rounded-md border-1 px-5 py-4 placeholder:text-gray-600 focus:border-1 focus:border-main focus:outline-none md:max-w-110 xl:max-w-160 xl:py-[22px]';
 
 export default Input;
