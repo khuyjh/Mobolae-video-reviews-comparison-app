609ca789267ea8479137aa047b80b2fd54b25b26
KimIn <inptp1996@gmail.com>
2025-09-26 15:57:56 +0900

    [HOY-190]fix: woff 폴백 처리 및 400,600 굵기만 남기고 제거(김인)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ import '@/styles/globals.css';
 const pretendard = localFont({
   src: [
     {
-      path: '../../public/fonts/Pretendard-Light.woff2',
-      weight: '300',
+      path: '../../public/fonts/Pretendard-Regular.woff',
+      weight: '400',
       style: 'normal',
     },
     {
@@ -24,18 +24,13 @@ const pretendard = localFont({
       style: 'normal',
     },
     {
-      path: '../../public/fonts/Pretendard-Medium.woff2',
-      weight: '500',
+      path: '../../public/fonts/Pretendard-SemiBold.woff',
+      weight: '600',
       style: 'normal',
     },
     {
       path: '../../public/fonts/Pretendard-SemiBold.woff2',
       weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../../public/fonts/Pretendard-Bold.woff2',
-      weight: '700',
       style: 'normal',
     },
   ],
